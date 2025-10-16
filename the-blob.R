19869615b36bf522496e27d5c7d6f8579d7a023a
samanthacsik <samanthacsik@gmail.com>
2025-10-15 21:34:21 -0700

    some updates

--- the-blob.R
+++ the-blob.R
@@ -2,19 +2,20 @@
 library(tidyverse)
 
 # load data ----
-temp_data <- read_csv(here::here("sci_temps.csv"))
+temp_data <- read_csv(here::here("data", "sci_temps.csv"))
 
 # calculate mean temp by day ----
 temp_summary <- temp_data |> 
-  group_by(year, doy) |> 
+  group_by(date, doy) |> 
   summarize(mean_temp = mean(temp_c)) 
 
 # plot data ---
 ggplot(temp_by_day_sci, aes(x = doy, y = mean_temp, group = year)) + 
   geom_line(alpha = 0.75, linewidth = 0.75) +
-  gghighlight::gghighlight(year == 2015) +
-  labs(
-    x = "Day of Year",
-    y = "Temperature (°C)"
-  ) +
-  theme_minimal()
+  gghighlight::gghighlight(temp_c > 22)
+  #gghighlight::gghighlight(year == 2015) #+
+  # labs(
+  #   x = "Day of Year",
+  #   y = "Temperature (°C)"
+  # ) +
+  # theme_minimal()
